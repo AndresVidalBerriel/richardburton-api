9c3b50f56ffa599a54224233046caa90e5912ba6
andres-vidal <andresvidalberriel@gmail.com>
2020-01-07 02:06:35 -0300

    Add getter/setter for User id

--- src/main/java/br/edu/ifrs/canoas/transnacionalidades/richardburton/entities/User.java
+++ src/main/java/br/edu/ifrs/canoas/transnacionalidades/richardburton/entities/User.java
@@ -129,4 +129,13 @@ public class User {
     public void setToken(String token) {
         this.token = token;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
