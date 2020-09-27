ae6c8cc26a3511a52666422b7975fbabbcb335dc
andresvidalberriel <andresvidalberriel@gmail.com>
2020-09-27 19:47:37 -0300

    Define UserInvitation resource

---
+++ src/main/java/br/edu/ifrs/canoas/richardburton/users/UserInvitationResource.java
@@ -0,0 +1,40 @@
+package br.edu.ifrs.canoas.richardburton.users;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("/users/invitations")
+public interface UserInvitationResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    Response retrieve();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response create(UserInvitation invitation);
+
+    @GET
+    @Path("/{email}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response retrieve(@PathParam("email") String email);
+
+    @POST
+    @Path("/confirmed")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response confirm(
+      @HeaderParam("RB-authentication-string") String authenticationString,
+      User user
+    );
+
+    @POST
+    @Path("/cancelled")
+    @Consumes(MediaType.TEXT_PLAIN)
+    Response cancel(String email);
+
+}
